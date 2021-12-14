f13b726e3d9182e8b72bba170f5492abac716b09
Rusty <enigma994@live.co.uk>
2021-12-14 11:00:56 +0000

    Changed back-end URL

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'http://quizapiiaj-env.eba-5b8k6f7s.us-east-2.elasticbeanstalk.com/';
 
     return service;
 }]);
